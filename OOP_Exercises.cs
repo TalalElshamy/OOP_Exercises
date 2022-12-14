d1c8cf32169bbe086d870fddb9aa7b31df0f5774
TalalElshamy <116369879+TalalElshamy@users.noreply.github.com>
2022-12-14 20:52:29 +0200

    Add files via upload

---
+++ OOP_Exercises.cs
@@ -0,0 +1,10 @@
+using System;
+namespace Talal{
+	class OOP_Exercises{
+	  public static void Main(String []arge){
+		  Console.WriteLine("OOP Exercises");
+		  Console.WriteLine("Talal Alaa Elshamy");
+
+	  }
+	}
+}
